849c5e74551db2754029b7fed4119e5ccc07a88e
Aditya Ghule <aghule@deqode.com>
2021-05-21 18:24:23 +0530

    Changed deploy calls according to contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,6 +4,6 @@ const YraceSeedMaster = artifacts.require("YraceSeedMaster");
 module.exports = async function (deployer) {
   deployer.deploy(YraceToken);
   const YraceTkn = await YraceToken.deployed();
-  deployer.deploy(YraceSeedMaster,YraceTkn.address,100,5);
+  deployer.deploy(YraceSeedMaster,YraceTkn.address,100,5,100,10000);
   console.log(YraceTkn.address);
 };
