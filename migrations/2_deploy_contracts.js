31d55061cafd5f22a18b4adaeaabce9505d93a25
Aditya Ghule <aghule@deqode.com>
2021-06-02 17:29:26 +0530

    Updated migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,6 +4,6 @@ const YraceSeedMaster = artifacts.require("YraceSeedMaster");
 module.exports = async function (deployer) {
   deployer.deploy(YraceToken);
   const YraceTkn = await YraceToken.deployed();
-  deployer.deploy(YraceSeedMaster,YraceTkn.address,100,5,100,10000);
+  deployer.deploy(YraceSeedMaster,YraceTkn.address,100,5,100,10000,YraceTkn.address);
   console.log(YraceTkn.address);
 };
